b615aab4300991733b5a298e5159b0a4375a744d
Andres Mendoza <andres.david.mm@hotmail.com>
2023-09-06 21:46:37 -0500

    first commit

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,7 +1,8 @@
 const express = require('express');
+const genreRouter = require('./genre.router');
 const router = express.Router();
 
 // colocar las rutas aquí
-
+router.use('/genres', genreRouter);
 
 module.exports = router;
